10827cff9dcedf3ac23073df248b9be9a1f842d2
hyosoek <tennfin1@gmail.com>
2024-11-08 10:10:20 +0900

    Init

---
+++ src/main/java/com/example/jenkinspipeline/Calculator.java
@@ -0,0 +1,8 @@
+package com.example.jenkinspipeline;
+
+public class Calculator {
+
+    public Integer sum(Integer a, Integer b){
+        return a+b;
+    }
+}
---
+++ src/main/java/com/example/jenkinspipeline/CalculatorController.java
@@ -0,0 +1,4 @@
+package com.example.jenkinspipeline;
+
+public class CalculatorController {
+}
